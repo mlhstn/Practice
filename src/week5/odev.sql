4b40bde532a734aeea9c4120523ed5dac22ac026
melih üstün <melih.stn98@gmail.com>
2024-05-29 00:39:36 +0300

    ödev eklendi

--- src/week5/odev.sql
+++ src/week5/odev.sql
@@ -19,7 +19,7 @@
      rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
   *) SELECT * FROM film WHERE length < 50 AND rental_rate != 2.99 OR rental_rate != 4.99;
 
-  ------------------------------ ÖDEV 2  --------------------------
+  ------------------------------  ÖDEV 2  --------------------------
 
   1) film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit
      ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
@@ -35,3 +35,42 @@
   4) film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99
      olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
   *) SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
+
+  ------------------------------  ÖDEV 3  --------------------------
+
+  1) country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri
+     ile sonlananları sıralayınız.
+  *) SELECT country FROM country WHERE country LIKE 'A%a';
+
+  2) country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri
+     ile sonlananları sıralayınız.
+  *) SELECT country FROM country WHERE country LIKE '%_____n';
+
+  3) film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T'
+     karakteri içeren film isimlerini sıralayınız.
+  *) SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+
+  4) film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan
+     büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+  *) SELECT*FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
